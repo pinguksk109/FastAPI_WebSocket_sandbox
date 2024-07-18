07b437fb110e419480953de98cd3e76413eca350
Keisuke Ota <k.ota109@outlook.com>
2024-07-19 01:20:52 +0900

    domain層の実装

---
+++ app/domain/Article.py
@@ -0,0 +1,3 @@
+class Article:
+    def __init__(self, content: str):
+        self.content = content
---
+++ app/domain/organization.py
@@ -0,0 +1,4 @@
+class Organization:
+    def __init__(self, limit: int, count: int):
+        self.limit = limit
+        self.count = count
